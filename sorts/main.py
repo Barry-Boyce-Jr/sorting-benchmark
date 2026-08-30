dda1a5b384c267b5d7ef1b39fe1ad43987cc5415
Barry Boyce <barryboycejr@gmail.com>
2026-08-30 13:03:20 -0400

    added CLA, read input file & write output and results files

--- sorts/main.py
+++ sorts/main.py
@@ -1,9 +1,15 @@
 # Barry Boyce Jr
 # 8/30/2026
 # project0
-# counter.py
+# main.py
 
 import sys
+import time
+import csv
+from counter import Counter
+from selection_sort import selection_sort
+from merge_sort import merge_sort, merge
+from pathlib import Path
 
 #error if usage incorrect
 if len(sys.argv) != 5:
@@ -20,3 +26,42 @@ result_file = sys.argv[4]
 if algorithm != "selection" and algorithm != "merge":
     print("algorithm must be 'merge' or 'selection'")
     sys.exit(1)
+
+#read input file
+with open(input_file, "r") as file:
+    input = [line.strip() for line in file]
+
+#counter and timer
+counter = Counter()
+start = time.perf_counter()
+
+#sorts
+if algorithm == "selection":
+    sorted = selection_sort(input, counter)
+
+if algorithm == "merge":
+    sorted = merge_sort(input, counter)
+
+end = time.perf_counter()
+elapsed = end - start
+
+# writes to output file
+with open(output_file, "w") as output:
+    for word in sorted:
+        output.write(word + "\n")
+
+file_exists = Path(result_file).exists()
+
+# write to results file
+with open(result_file, "a", newline = "") as csvfile:
+    writer = csv.writer(csvfile)
+    if not file_exists:
+        writer.writerow(["algorithm", "size", "comparisons", "time(secs)"])
+    writer.writerow([algorithm, len(input), counter.count, elapsed])
+         
+
+
+
+
+
+
